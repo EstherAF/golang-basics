f46022deb48052354c88307c01435c381b5b8aee
Esther Álvarez <alvarezfeijoo.esther@gmail.com>
2017-07-18 19:42:40 +0200

    Add another library and import it from hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/estheraf/golang-basics/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world " + stringutil.Reverse("321") + "\n")
 }
